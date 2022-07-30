dd6e0aa4d10b0c4f6a310405e99fdb30978d9a07
Shamim <mim24sd@gmail.com>
2022-07-30 16:01:59 +0430

    remove unnecessary comments

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//side panel style
-
 function openNav() {
   document.getElementById("mySidepanel").style.width = "200px";
 }
@@ -8,13 +6,9 @@ function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 }
 
-//task
-
 let task = { title: "" };
 
 document.getElementById("add-task-title").value = task.title;
-
-//show add or edit view
 
 if (Object.values(task).every((x) => x === null || x === "")) {
   document.getElementById("edit-task-header").style.display = "none";
@@ -22,8 +16,6 @@ if (Object.values(task).every((x) => x === null || x === "")) {
 } else {
   document.getElementById("add-task-header").style.display = "none";
 }
-
-//add task
 
 function addNewTask() {
   task.title = document.getElementById("add-task-title").value;
