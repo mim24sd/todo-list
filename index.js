0e18c9291009548911e4aa3e04ba2f3ac0bd16dd
Shamim <mim24sd@gmail.com>
2022-08-04 01:44:37 +0430

    delete unnecessary lines

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ function closeNav() {
 }
 
 const tasks = [];
-
 const addTaskButton = document.getElementById("add-task-button");
-
 addTaskButton.addEventListener("click", () => {
   const taskTitle = document.getElementById("task-title").value;
-
   tasks.push({
     id: tasks.length + 1,
     title: taskTitle,
