e13d8a135b0a5a7d6556e1cc4655f241a8dab39b
Shamim <mim24sd@gmail.com>
2022-08-04 17:46:24 +0430

    arrange the code to be standard

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-
 const openNavButton = document.getElementById("open-nav-button");
+const closeNavButton = document.getElementById("close-nav-button");
+const addTaskButton = document.getElementById("add-task-button");
+const taskTitleInput = document.getElementById("task-title-input");
+const tasks = [];
 
 openNavButton.addEventListener("click", () => {
   document.getElementById("mySidepanel").style.width = "200px";
 });
 
-const closeNavButton = document.getElementById("close-nav-button");
-
 closeNavButton.addEventListener("click", () => {
   document.getElementById("mySidepanel").style.width = "0";
 });
-
-const tasks = [];
-const addTaskButton = document.getElementById("add-task-button");
-const taskTitleInput = document.getElementById("task-title-input");
 
 addTaskButton.addEventListener("click", () => {
   tasks.push({
