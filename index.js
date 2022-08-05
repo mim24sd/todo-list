ea6a1bbd78dfe727df8d73dea824b82777aaea1c
Shamim <mim24sd@gmail.com>
2022-08-05 15:12:28 +0430

    add created time to tasks

--- index.js
+++ index.js
@@ -13,11 +13,35 @@ closeNavButton.addEventListener("click", () => {
   sideMenuContainer.style.width = "0";
 });
 
+function addExtraZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+function getTime() {
+  var date = new Date();
+  var day = date.getDay();
+  var month = date.getMonth();
+  var year = date.getFullYear();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var second = date.getSeconds();
+
+  minute = addExtraZero(minute);
+  second = addExtraZero(second);
+  return year + "/" + month + "/" + day + "(" + hour + ":" + minute + ":" + second + ")";
+}
+
 addTaskButton.addEventListener("click", () => {
+  
+
   tasks.push({
     id: tasks.length + 1,
     title: taskTitleInput.value,
     isDone: false,
+    createdAt: getTime(),
   });
 
   console.log(tasks);
