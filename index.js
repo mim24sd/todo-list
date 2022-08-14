9fbc1c4a6fa659eca746e00ea0c477ce81ca5cef
Shamim <mim24sd@gmail.com>
2022-08-14 15:42:31 +0430

    change list structure

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const closeNavButton = document.getElementById("close-nav-button");
 const addTaskButton = document.getElementById("add-task-button");
 const taskTitleInput = document.getElementById("task-title-input");
 const taskTable = document.getElementById("task-table");
-const TaskItems = document.createElement("tr");
 const tasks = [];
-
-TaskItems.className = "table-box-row";
 
 openNavButton.addEventListener("click", () => {
   sideMenuContainer.style.width = "200px";
@@ -29,24 +26,24 @@ addTaskButton.addEventListener("click", () => {
     const taskCreatedAt = Object.values(task)[3]
       .toString()
       .match(/\d\d:\d\d:\d\d/);
+    const taskItems = document.createElement("task-table");
+    taskItems.className = "table-box-row";
 
-    TaskItems.innerHTML = `<td class="table-box-color-tag-job"> 
-      </td> 
-      <td class="table-box-task" id="show-task-title"> 
+    taskItems.innerHTML = `
+      <li class="table-box-task" id="show-task-title"> 
       ${Object.values(task)[1]} 
-      </td> 
-      <td class="table-box-time" id="show-task-time"> 
+      </li> 
+      <li class="table-box-time" id="show-task-time"> 
       ${taskCreatedAt} 
-      </td> 
-      <td class="table-box-check-box"> 
+      </li> 
+      <li class="table-box-check-box"> 
       <input
           class="table-box-check"
           type="checkbox"
           id="checkbox-done"
           name="checkbox-done"
         /> 
-      </td>`;
-
-    taskTable.append(TaskItems);
+      </li>`;
+    taskTable.append(taskItems);
   });
 });
