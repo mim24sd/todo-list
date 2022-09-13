926b06c2bdf70246c1f0fdbe3e429d2e0a49ca0a
Shamim <mim24sd@gmail.com>
2022-09-13 15:10:49 +0430

    comment sort cause it was extra for now

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const closeNavButton = document.getElementById("close-nav-button");
 const addTaskButton = document.getElementById("add-task-button");
 const taskTitleInput = document.getElementById("task-title-input");
 const taskList = document.getElementById("task-list");
-const sortTaskDropdown = document.getElementById("sort-tasks");
+// const sortTaskDropdown = document.getElementById("sort-tasks");
 const sreachInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 const timeFilterDropdown = document.getElementById("filter-by-time-tasks");
@@ -36,12 +36,12 @@ searchButton.addEventListener("click", () => {
   filterTasksByTitle(sreachInput.value);
 });
 
-sortTaskDropdown.addEventListener("change", () => {
-  const sortType =
-    sortTaskDropdown.options[sortTaskDropdown.selectedIndex].value;
+// sortTaskDropdown.addEventListener("change", () => {
+//   const sortType =
+//     sortTaskDropdown.options[sortTaskDropdown.selectedIndex].value;
 
-  sortTasks(sortType);
-});
+//   sortTasks(sortType);
+// });
 
 timeFilterDropdown.addEventListener("change", () => {
   const selectedTime =
@@ -90,30 +90,30 @@ function createTaskCheckBox() {
           /> `;
 }
 
-function sortTasks(sortType) {
-  if (sortType == "byTitle") {
-    renderTasks(sortTasksByTitle());
-  } else {
-    renderTasks(unsortTasks());
-  }
-}
+// function sortTasks(sortType) {
+//   if (sortType == "byTitle") {
+//     renderTasks(sortTasksByTitle());
+//   } else {
+//     renderTasks(unsortTasks());
+//   }
+// }
 
-function sortTasksByTitle() {
-  const collator = new Intl.Collator("en", {
-    numeric: true,
-  });
+// function sortTasksByTitle() {
+//   const collator = new Intl.Collator("en", {
+//     numeric: true,
+//   });
 
-  return tasks.sort((randomTask1, randomTask2) =>
-    collator.compare(
-      randomTask1.title.toUpperCase(),
-      randomTask2.title.toUpperCase()
-    )
-  );
-}
+//   return tasks.sort((randomTask1, randomTask2) =>
+//     collator.compare(
+//       randomTask1.title.toUpperCase(),
+//       randomTask2.title.toUpperCase()
+//     )
+//   );
+// }
 
-function unsortTasks() {
-  return tasks.sort((task1, task2) => (task1.id > task2.id ? 1 : -1));
-}
+// function unsortTasks() {
+//   return tasks.sort((task1, task2) => (task1.id > task2.id ? 1 : -1));
+// }
 
 function filterTasksByTitle(text) {
   renderTasks(tasks.filter((task) => task.title.includes(text)));
