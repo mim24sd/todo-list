a0ac07db196ba904c402eaa68983f5a6721e5846
Shamim <mim24sd@gmail.com>
2022-09-14 22:35:28 +0430

    delete extra comments

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const closeNavButton = document.getElementById("close-nav-button");
 const addTaskButton = document.getElementById("add-task-button");
 const taskTitleInput = document.getElementById("task-title-input");
 const taskList = document.getElementById("task-list");
-// const sortTaskDropdown = document.getElementById("sort-tasks");
 const sreachInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 const timeFilterDropdown = document.getElementById("filter-by-time-tasks");
@@ -61,11 +60,6 @@ searchButton.addEventListener("click", () => {
   filterTasksByTitle(sreachInput.value);
 });
 
-// sortTaskDropdown.addEventListener("change", (sortType) => {
-//   sortTasks(sortType.target.value);
-
-// });
-
 timeFilterDropdown.addEventListener("change", (selectedTime) => {
   filterTasksByTime(selectedTime.target.value);
 });
@@ -109,31 +103,6 @@ function createTaskCheckBox() {
             name="checkbox-done"
           /> `;
 }
-
-// function sortTasks(sortType) {
-//   if (sortType == "byTitle") {
-//     renderTasks(sortTasksByTitle());
-//   } else {
-//     renderTasks(unsortTasks());
-//   }
-// }
-
-// function sortTasksByTitle() {
-//   const collator = new Intl.Collator("en", {
-//     numeric: true,
-//   });
-
-//   return tasks.sort((randomTask1, randomTask2) =>
-//     collator.compare(
-//       randomTask1.title.toUpperCase(),
-//       randomTask2.title.toUpperCase()
-//     )
-//   );
-// }
-
-// function unsortTasks() {
-//   return tasks.sort((task1, task2) => (task1.id > task2.id ? 1 : -1));
-// }
 
 function filterTasksByTitle(text) {
   renderTasks(tasks.filter((task) => task.title.includes(text)));
